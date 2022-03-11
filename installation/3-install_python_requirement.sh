41305d6617072f47bd66f75d2b160097b6ece60e
Hesam Haghpanah <43572130+h-haghpanah@users.noreply.github.com>
2022-03-11 12:55:58 +0330

    update requirements

--- installation/3-install_python_requirement.sh
+++ installation/3-install_python_requirement.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python3 -m venv /etc/mikrotik_traffic_counter_en/mikrotik_venv
-source ../mikrotik_venv/bin/activate
-pip3 install -r ../requirement.txt
+source /etc/mikrotik_traffic_counter_en/mikrotik_venv/bin/activate
+pip3 install -r /etc/mikrotik_traffic_counter_en/requirements.txt
 cp /etc/mikrotik_traffic_counter_en/services/* /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start mikrotik_analysis.service
